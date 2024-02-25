d88dd02ba021d32084aa4571b25c44d8e255ff9f
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-02-25 20:28:05 +0100

    initial commit

---
+++ PogSubSharp/Models/PubSubFrame.cs
@@ -0,0 +1,9 @@
+﻿namespace PogSubSharp.Models;
+
+public class PubSubFrame
+{
+    public PubSubMessageType Type { get; set; }
+    public string? Nonce { get; set; }
+    public string? Data { get; set; }
+    
+}
---
+++ PogSubSharp/Models/PubSubMessageType.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PogSubSharp.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum PubSubMessageType
+{
+    PING,           // => "PING"
+    PONG,           // => "PONG"
+    RECONNECT,      // => "RECONNECT"
+    AUTH_REVOKED,   // => "AUTH_REVOKED"
+    LISTEN,         // => "LISTEN"
+    UNLISTEN,       // => "UNLISTEN"
+    MESSAGE         // => "MESSAGE"
+}
